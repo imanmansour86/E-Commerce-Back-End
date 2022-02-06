1e0b4beb1ab02deb485d7cbbdbffc13053b50518
imanmansour86 <imanmansour86@gmail.com>
2022-02-05 20:49:00 -0800

    adding post tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,15 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
